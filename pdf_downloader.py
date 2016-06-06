a9ebc595794dcf261d47c50d312dc2ba23f2c0d8
Akash Thakur <skyatwork007@gmail.com>
2016-06-06 20:45:06 +0530

    Add files via upload

---
+++ pdf_downloader.py
@@ -0,0 +1,22 @@
+import urllib
+import requests
+
+
+roomnumber = 000
+origin = 'http://www.srmuniv.ac.in/downloads/cdc/batch3/room_no_'
+
+for i in range (000,1000):
+	print("searching room number "+ str(i))
+	url = origin +str(i)+'.pdf'
+	r= requests.get(url)
+	print(r.status_code)
+	if(r.status_code == 200):
+		print(" found room number " + str(i))
+		urllib.urlretrieve(url , "/home/seintern/Desktop/pdfs/student_UB_room_number_"+str(i)+".pdf")
+
+
+
+
+
+
+
